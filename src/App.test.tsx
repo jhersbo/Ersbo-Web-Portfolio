bcaf8848f1295ba048078b998a712996b1e4dba9
jhersbo <jhersbo@gmail.com>
2022-09-20 18:27:17 -0500

    more testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import renderer from 'react-test-renderer'
 
-test('renders a snapshot', () => {
+test('passes snapshot test', () => {
   const tree = renderer.create(<App/>).toJSON()
   expect(tree).toMatchSnapshot()
 });
--- src/components/HeroNav.test.tsx
+++ src/components/HeroNav.test.tsx
@@ -1,1 +1,7 @@
 import HeroNav from "./HeroNav";
+import renderer from 'react-test-renderer'
+
+test('passes snapshot test', () => {
+  const tree = renderer.create(<HeroNav/>).toJSON()
+  expect(tree).toMatchSnapshot()
+});
