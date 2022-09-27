96ae6d3c781343c5418db5d686051b35e66980c2
jhersbo <jhersbo@gmail.com>
2022-09-27 11:53:51 -0500

    setting up

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,33 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 
 import HeroNav from './components/HeroNav';
+import LandingScreen from './components/LandingScreen';
+
 import { ReactNode } from 'react';
 import React from 'react';
 
-class App extends React.Component{
+class App extends React.Component<{}, { page: string }>{
+  constructor(props: any){
+    super(props);
+    this.state = {
+      page: 'landing',
+    }
+    this.setView = this.setView.bind(this)
+  }
+  setView = (page: string)=>{
+    this.setState({page: page})
+  }
   render(): ReactNode {
-    return (
-      <div className="App">
-        <Router>
-          <HeroNav></HeroNav>
-          <Routes>
-  
-          </Routes>
-        </Router>
-      </div>
-    );
+    if(this.state.page === 'landing'){
+      return(
+        <LandingScreen/>
+      )
+    }else{
+      return(
+        <HeroNav/>
+        //other stuff
+      )
+    }
   }
 }
 
---
+++ src/components/LandingScreen.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+class LandingScreen extends React.Component{
+    constructor(props: {} | Readonly<{}>){
+        super(props)
+    }
+    render(): React.ReactNode {
+        return(
+            <div>
+                <h1>Hello</h1>
+            </div>
+        )
+    }
+}
+
+export default LandingScreen
